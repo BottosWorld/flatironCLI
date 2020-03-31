0fd506722df5781f493c1e1db105aba5604018aa
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-31 17:22:36 +0000

    add notes for CLI

--- lib/recipeCLI/cli.rb
+++ lib/recipeCLI/cli.rb
@@ -1,8 +1,33 @@
 class RecipeCLI::CLI
   
+  #Give comments of description
+  #Start should greet user and give description
+  
+  #What are we trying to do with our application?
+  #Get data from api/scraper
+  #create new custom objects
+  #all within start of app
+  
   def start
     puts "Hello there!"
-    binding.pry
+    puts "Getting Data from API... Please Wait"
+    puts "Loading Objects.."
+    #binding.pry
   end
+  
+  #BULK OF OUR CODE:
+    #deal with inputs (loop that keeps asking for new info)
+    #ex -> while input does not equal "EXIT" do 
+    
+    #display a list of options for users to select
+    #get user input
+    #do something depending on input
+    #if condition to check if input is valid
+    #else condition to tell user "try again"
+  
+  #exit command
+  #if input == "EXIT"
+  #kill program, say "goodbye"
+
   
 end
