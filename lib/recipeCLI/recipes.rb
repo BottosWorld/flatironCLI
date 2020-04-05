d4567f9a22fc43e0c35198919db8a1584963f4ac
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-04-05 22:13:27 +0000

    add code to recipes file, objects for mealtype, ingr, and time

--- lib/recipeCLI/recipes.rb
+++ lib/recipeCLI/recipes.rb
@@ -1,5 +1,19 @@
 class RecipeCLI::Recipes
+  attr_accessor :ingr :mealtype :time
+  @@all = []
   
+  def initialize(hash)
+    hash.each {|key, value| self.send(("#{key}="), value)}
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
   
   
   
